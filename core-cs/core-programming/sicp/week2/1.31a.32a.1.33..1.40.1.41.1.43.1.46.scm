8b3e4ac18393b97e66932f1f06b30adf07ec858d
Andro <ranogaet@gmail.com>
2024-07-19 20:39:46 +0300

    cs61a week2 hw 3/4 done, calc 1a diff, inverse function done

--- core-cs/core-programming/sicp/week2/1.31a.32a.1.33..1.40.1.41.1.43.1.46.scm
+++ core-cs/core-programming/sicp/week2/1.31a.32a.1.33..1.40.1.41.1.43.1.46.scm
@@ -114,31 +114,31 @@
 ;      #f
 ;      #t))
 
-(define (prime? p)
+(define (prime? a p)
   (define (fermat-little-theorem? next a p)
     (cond
       ((= a (- p 1))#t)
       ((not (= (remainder (expt a (- p 1)) p) 1))#f)
       (else
        (fermat-little-theorem? next (next a) p))))
-  (fermat-little-theorem? inc 1 p))
+  (fermat-little-theorem? inc a p))
 
 ;(trace prime?) 
-;(prime? 31)
-;(prime? 30)
+;(prime? 1 31)
+;(prime? 1 30)
 
 ;1.33a.p2
 (define (filtered-accumulate predicate combiner null-value term a next b)
   (if (> a b)
       null-value
-      (combiner (if(predicate a)
+      (combiner (if(predicate a b)
                     (term a)
                      null-value)
                 (filtered-accumulate predicate combiner null-value term (next a) next b))))
 
-(define (sum-prime-squares-filtered b)
-  (filtered-accumulate prime? + 0 square 1 inc b))
-;(sum-prime-squares-filtered 10)
+(define (sum-prime-squares-filtered  a b)
+  (filtered-accumulate prime? + 0 square a inc b))
+;(sum-prime-squares-filtered 1 10)
 
 
 ;1.33b
@@ -158,9 +158,119 @@
     ((and(> a b)(=(remainder a b)0)(=(remainder b a)b))b)
     ((> a b)(gcd (remainder a b)b))
     ((< a b)(gcd a(remainder b a)))))
-        
 
-(gcd 0 5)
-        
+(define (gcd-prime? a b)
+  (if (and (< a b)(= (gcd a b) 1))
+      #t
+      #f))
   
+ 
+(define (product-relative-prime-integers a b)
+  (filtered-accumulate gcd-prime? * 1 identity a inc b))
+
+(trace gcd-prime?)
+;(product-relative-prime-integers 1 5)
+
+
+;1.40
+;herons method 
+(define tolerance 0.00001)
+(define dx 0.00001)
+(define (average a b)
+  (/(+ a b)2.0))
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (heron-update x S)
+  (average x (/ S x)))
+
+;(define (heron-sqrt x)
+; (fixed-point (average-damp (lambda (y) (/ x y)))1.0))
+;(define (average-damp f)
+;  (lambda (x) (average  x (f x))))
+
+
+(define (heron-sqrt S)
+  (fixed-point (lambda (x) (heron-update x S)) 1.0))
+
+;(heron-sqrt 2)
+
+(define (deriv g)
+  (lambda (x) (/ (- (g (+ x dx)) (g x)) dx)))
+
+(define (newton-transform g)
+  (lambda (x) (- x (/ (g x) ((deriv g) x)))))
+
+(define (newton-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (newton-sqrt S)
+  (newton-method (lambda (x)(-(square x)S)) 1.0))
+
+;(newton-sqrt 2)
+
+
+(define (cubic a b c)
+  (lambda (x) (+ (expt x 3) (* a (expt x 2)) (* b x) c)))
+
+;((deriv (cubic 2 2 3))1.0)
+
+;((cubic 2 2 3)1.0)
+
+;(newton-method (cubic 2 2 3) 1.0)
+
+
+;1.41
+;1call single with inc as a callback
+;1-return the lambda where f is bound to inc
+;2-apply the return lambda function to 2
+
+
+(define (single f)
+  (lambda (x)(f x)))
+
+(define (single1 f x)
+  ((lambda (x)(f x))x))
+
+
+;((single inc)2)
+;(single1 inc 2)
+
+(define (double f)
+  (lambda (x)(f(f x))))
+
+;(trace double)
+
+;(((double double) inc)5)
+
+;(((double(double double))inc)5)
+
+;1.43
+;recursively call helper in returns a lambda,
+;which applies the outer x i.e 5 to the other lamba
+;in which f applies to x and pass it to the next scope
+
+(define (repeated f n)
+    (if (< n 1)
+       (lambda (x) x)
+       (lambda (x)(f ((repeated f (- n 1)) x)))))
+
+((repeated square 2)5)
+
+(define (repeated1 f n v)
+  (if (< n 1)
+      v
+      (f(repeated1 f (- n 1) v))))
+
+(repeated1 square 3 5)
+
 
