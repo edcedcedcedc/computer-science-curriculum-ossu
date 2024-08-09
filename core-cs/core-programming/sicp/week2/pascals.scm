2b5b81025414e7bad975dbf20a515fcce7149287
Andro <ranogaet@gmail.com>
2024-08-09 13:14:26 +0300

    mit calculus 1a unit 3 approximation for me is useless it contains linear approximations with too much physics, trigonometry whichsenselessly makes the material harder to grasp, so I am going to skip it and loop up on Khan Academy or practice more calculus 1a by taking classical mechanics

---
+++ core-cs/core-programming/sicp/week2/pascals.scm
@@ -0,0 +1,10 @@
+#lang simply-scheme
+
+
+(define (pascal row col)
+  (cond ((= col 0) 1)
+        ((= col row) 1)
+        (else (+ (pascal (- row 1) (- col 1))
+                 (pascal (- row 1) col) ))))
+(trace pascal)
+(pascal 3 2)
