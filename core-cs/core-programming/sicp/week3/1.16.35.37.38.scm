ba8dffb2968791bcff616e466631800b77588916
Andro <ranogaet@gmail.com>
2024-08-18 16:31:22 +0300

    cs61a/sicp week 3 ex 1.16.35.37.38 done, refactor cont-frac-rec from descending recursion to ascending recursion

--- core-cs/core-programming/sicp/week3/1.16.35.37.38.scm
+++ core-cs/core-programming/sicp/week3/1.16.35.37.38.scm
@@ -46,22 +46,50 @@
 ; (golden-ratio 10.0)
 
 ;1.37
+;
 ;about 11 recursive calls to get to 4 decimal places of the actual golden ratio 
-
 ;recursive definition
+
 (define (cont-frac-rec n d k)
-  (if (= k 0)
-      1.0
-      (/ (n k)(+ (d k) (cont-frac-rec n d (- k 1))))))
+  (define (recur-ascend i)
+    (if (> i k)
+        0
+        (/ (n i)(+ (d i) (recur-ascend (+ i 1))))))
+  (recur-ascend 1))
 
-;(trace cont-frac-rec)
-(cont-frac-rec (lambda(i)1.0)(lambda(i)1.0) 11)
+(define (cont-frac-iter n d k)
+  (define (iter k a)
+    (if (= k 0)
+        a
+        (iter (- k 1) (/ (n k) (+ (d k) a )))))
+  (iter k 0))
 
-(define (cont-frac-iter n d k a)
-  (if (= k 0)
-      a
-      (cont-frac-iter n d (- k 1) (/ (n k)(+ (d k) a )))))
+;1.38
+;understanding/goal:
+;1 2  1 1 4  1 1 6  1 1 8  1 1 10  1 1 12  1 1 14...
+;0 1  2 3 4  5 6 7  8 9 10 - i 
+;1 2  3 4 5  6 7 8  8 10 11 - i + 1
+;strategy:
+;a = 0 
+;iter where i starts from 0, 0 1 2 => 1 2 3 i.e i => i - 1
+;if i mod 3 = 1 => a + 2
+;else a
+;
+;
+(define (euler-d i)
+  (define (iter i a)
+    (if (= i 0)
+        a
+        (iter (- i 1)
+                 (if (= (remainder i 3) 1)
+                     (+ a 2)
+                     a))))
+  (if (= (remainder (- i 1) 3) 1)
+      (iter (- i 1) 0)
+      1))
 
-(cont-frac-iter (lambda(i)1.0)(lambda(i)1.0) 11 1.0)
+(define (approx-e k)
+  (+ (cont-frac-rec (lambda (i) 1.0)
+                    euler-d k)2))
 
-
+(approx-e 10)
