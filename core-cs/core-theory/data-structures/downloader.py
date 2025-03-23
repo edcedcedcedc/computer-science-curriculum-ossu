103d05ea04b26db36f3360bdf96cd5103673d375
edc <ranogaet@gmail.com>
2025-03-23 15:19:48 +0200

    Data Structures/Downloader: Project2/Rewrote it to be recursive

--- core-cs/core-theory/data-structures/downloader.py
+++ core-cs/core-theory/data-structures/downloader.py
@@ -20,44 +20,82 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
+
+updates:
+23/03/2025 - recursively lookup n-nested folders and download files
+            NOTES:
+                This update is very specific and is searching folder under "folder/" and files undr "files",
+                For this to be more generalized, you need a more advanced match for "folder/".
+            FOR EXAMPLE:
+                Now it will download a folder which has inside n-nested folders as "folder/" but not as "folder"
+            
+
 """
 import os
 import sys
 import requests
 from bs4 import BeautifulSoup
 
+def download(output_dir, response, url):
+    soup = BeautifulSoup(response.text, 'html.parser')
+    print(output_dir, response, url)
+    for link in soup.find_all('a'):
+        file_name = link.get('href')
+        print('# Current filename: ', file_name)
+        file_url = url + file_name
+
+        if (file_name in ("../", "/") or 
+           file_name.startswith("?") or 
+           file_name.startswith("/")):
+            continue
+        
+        elif file_name.endswith('/'):
+            try:
+                new_output_dir = output_dir + "/" + file_name.strip("/")
+                new_url = file_url
+
+                print(f"# New output directory: ", new_output_dir)
+                print(f"# New output url: ", new_url)
+                os.makedirs(new_output_dir, exist_ok=True)
+                
+                response = requests.get(new_url)
+                if response.status_code == 200:
+                    print("# Recursively calling myself...")
+                    print(new_output_dir, response, new_url)
+                    download(new_output_dir, response, new_url)
+                    continue  
+                else:
+                    raise Exception(f"# Response status code: {response.status_code}")
+                    
+            except Exception as e:
+                        print(e); exit()
+
+        print(f"# Downloading {file_name}...")
+
+        try:
+            file_response = requests.get(file_url)
+            file_response.raise_for_status()
+           
+            with open(os.path.join(output_dir, file_name), 'wb') as f:
+                f.write(file_response.content)
+                
+        except Exception as e:
+            print(f"# Failed to download, {e}, filename: {file_name}")
+
+    print("# All files downloaded.")
+   
 try:
     url = f"{sys.argv[1]}"
     output_dir = f"{sys.argv[2]}"
-
+    print("# Output directory: ", output_dir)
     os.makedirs(output_dir, exist_ok=True)
-
     response = requests.get(url)
 
-except Exception as e:
-    print(e); exit()
-
-soup = BeautifulSoup(response.text, 'html.parser')
-
-for link in soup.find_all('a'):
-    file_name = link.get('href')
-    
-
-    if file_name in ("../", "/") or file_name.endswith('/') or file_name.startswith("?"):
-        continue
-    
-    file_url = url + file_name
-    print(f"Downloading {file_name}...")
-    
-    try:
-
-        file_response = requests.get(file_url)
-        file_response.raise_for_status()
-        
-        with open(os.path.join(output_dir, file_name), 'wb') as f:
-            f.write(file_response.content)
+    if response.status_code == 200:
+        download(output_dir, response, url)
+    else:
+        raise Exception(f"# Wrong formatting(use: <functionName link 'path/to/save'>) or dead link, response status code: {response.status_code}")
             
-    except Exception as e:
-        print(f"Failed to download, {e}, filename: {file_name}")
-
-print("All files downloaded.")
+except Exception as e:
+            print(e); exit()
