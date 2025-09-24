5cb38b41b93f3e4b19278b5251455baf217a898c
edc <ranogaet@gmail.com>
2025-09-24 18:22:31 +0300

    Probability for cs: ps1 counting, exercises

---
+++ advanced-cs/advanced-math/probability-for-cs/ps1-counting/permutationnondistinct.py
@@ -0,0 +1,6 @@
+# permute boba, as a non distinct string;
+# I took this from the lecture, will code it later when I forget a little and will think more
+
+import math
+
+print(math.factorial(4) / math.factorial(2))
